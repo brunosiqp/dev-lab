501b0cab042c5219fcf552d567af92453dd0c861
Bruno <b.s.parente@gmail.com>
2026-03-05 17:02:55 -0300

    Create languageMap.js

---
+++ experiments/github-language-index/languageMap.js
@@ -0,0 +1,21 @@
+const languageMap = {
+  ".js": "javascript",
+  ".ts": "typescript",
+  ".py": "python",
+  ".java": "java",
+  ".go": "go",
+  ".rs": "rust",
+  ".php": "php",
+  ".cs": "csharp",
+  ".cpp": "cpp",
+  ".c": "c",
+  ".html": "html",
+  ".css": "css",
+  ".sql": "sql",
+  ".json": "json",
+  ".yaml": "yaml",
+  ".yml": "yaml",
+  ".md": "markdown"
+}
+
+module.exports = languageMap
